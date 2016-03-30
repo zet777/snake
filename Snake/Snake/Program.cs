d6cc86a048da2dbd80176c47091e5a18a0490a28
rzet777 <rzet777@gmail.com>
2016-03-30 16:46:33 +0300

    Эксперименты с классом List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,12 +10,31 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');       
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point(4,5,'#'); 
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
-           
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
      
         }
