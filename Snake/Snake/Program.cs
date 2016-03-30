597e1e22a2a8287e263509ae1c6ead9e11966d4b
rzet777 <rzet777@gmail.com>
2016-03-30 17:21:26 +0300

    Горизонтальная линия.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,44 +16,9 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(5, 6, '#');
-            p3.Draw();
-
-            Point p4 = new Point(6, 7, '#');
-            p4.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-            
-
-            numList.RemoveAt(0);
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            List<char> symList = new List<char>();
-            symList.Add('!');
-            symList.Add('$');
-            symList.Add('%');
-            symList.Add('^');
-
-            foreach (char i in symList)
-            {
-                Console.WriteLine(i);
-            }
-
+            HorizontalLine line = new HorizontalLine(5,10,8, '+');
+            line.Drow();
+  
             Console.ReadLine();
      
         }
