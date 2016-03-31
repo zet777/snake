9e961f05fbf200deb2ea1406e18bbb33af63e22a
rzet777 <rzet777@gmail.com>
2016-03-31 17:44:53 +0300

    Змейка реагирует на нажатия клавиш.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -31,24 +31,23 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
 
-            Console.ReadLine();
+            while(true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+
+                }
+                Thread.Sleep(100);
+                snake.Move();
+
+            }
+            
+          
+
+            
 
         }
       
